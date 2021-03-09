9bb07dfce64f3e6df7d5891ec04af9c29ef18ab5
jmaydew <jmaydew@student.42.fr>
2021-03-09 17:16:28 +0100

    nginx depl updates + metallb config

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-#minikube start
-eval $(minikube -p minikube docker-env) #in order for minikube to look for docker images locally
+#stopping nginx on localhost
+nginx -s stop
 
+#minikube delete
+#minikube start --driver=docker
+eval $(minikube -p minikube docker-env) #in order for minikube to look for docker images locally
 
 #image build
 
@@ -10,4 +13,4 @@ docker build nginx/ -t nginx_service	 #nginx image build
 
 kubectl apply -f nginx/depl_nginx.yaml #nginx service deployment
 
-#minikube dashboard	&#open kubernetes dashboard
+minikube dashboard & #open kubernetes dashboard
