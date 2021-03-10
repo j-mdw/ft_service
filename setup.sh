75c857c364c45ed503e0b500606f77ce860d2c61
jmaydew <jmaydew@student.42.fr>
2021-03-10 12:02:38 +0100

    +TSL certificates generation +wordpress container image in process

--- setup.sh
+++ setup.sh
@@ -3,14 +3,21 @@
 #stopping nginx on localhost
 nginx -s stop
 
+#creating TLS certificates
+mkdir certs
+cd certs
+openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -batch -out cert.pem -keyout key.pem
+chmod 400 key.pem
+cp *.pem ../nginx/srcs
+cd ..
 #minikube delete
 #minikube start --driver=docker
-eval $(minikube -p minikube docker-env) #in order for minikube to look for docker images locally
+##eval $(minikube -p minikube docker-env) #in order for minikube to look for docker images locally
 
 #image build
 
-docker build nginx/ -t nginx_service	 #nginx image build
+##docker build nginx/ -t nginx-service	 #nginx image build
 
-kubectl apply -f nginx/depl_nginx.yaml #nginx service deployment
+##kubectl apply -f nginx/depl_nginx.yaml #nginx service deployment
 
-minikube dashboard & #open kubernetes dashboard
+##minikube dashboard & #open kubernetes dashboard
