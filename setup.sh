7303461c7df6204f1efb2fd5b74329ec7c6b98ac
jmaydew <jmaydew@student.42.fr>
2021-03-12 17:22:09 +0100

    metallb install and config

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #stopping nginx on localhost
-nginx -s stop
+##sudo nginx -s stop
 
 #creating TLS certificates
 mkdir certs
@@ -10,16 +10,21 @@ openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -batch -out cer
 chmod 400 key.pem
 cp *.pem ../nginx/srcs
 cd ..
-#minikube delete
-#minikube start --driver=docker
-#eval $(minikube -p minikube docker-env) #in order for minikube to look for docker images locally
+
+#Minikube setup
+##minikube delete
+##minikube start --driver=docker
+##eval $(minikube -p minikube docker-env) #in order for minikube to look for docker images locally
 
 #image build
+#docker build nginx/ -t nginx-service	 #nginx image build
+#docker build wordpress/ -t wordpress-service
+#docker build phpmyadmin/ -t phpmyadmin-service
+#docker build mysql/ -t mysql-volume
 
-docker build nginx/ -t nginx-service	 #nginx image build
-docker build wordpress/ -t wordpress-service
-docker build phpmyadmin/ -t phpmyadmin-service
-
+docker run --rm -d -p 80:80 -p 443:443 nginx-service
+docker run --rm -d -p 5050:5050 wordpress-service
+docker run --rm -d -p 5000:5000 phpmyadmin-service
 ##kubectl apply -f nginx/depl_nginx.yaml #nginx service deployment
 
 ##minikube dashboard & #open kubernetes dashboard
