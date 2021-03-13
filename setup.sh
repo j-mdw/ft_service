d12031e7cd44cb991d5f8e55f71be5f5e15d1608
jmaydew <jmaydew@student.42.fr>
2021-03-13 13:18:41 +0100

    + ext IP capture

--- setup.sh
+++ setup.sh
@@ -1,33 +1,62 @@
 #!/bin/sh
 
-#stopping nginx on localhost
-#sudo nginx -s stop
+#Directories (relative path)
 
-#creating TLS certificates
-#mkdir certs
-#cd certs
-#openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -batch -out cert.pem -keyout key.pem
-#chmod 400 key.pem
-#cp *.pem ../nginx/srcs
-#cd ..
+mlb_dir="metallb/"
+ngx_dir="nginx/"
+wop_dir="wordpress/"
+pma_dir="phpmyadmin/"
+msq_dir="mysql/"
+
+#files (relative path)
+
+mlb_yaml="$mlb_dir""metallb.yaml"
+ngx_yaml="$ngx_dir""nginx.yaml"
+wp_yaml="$wp_dir""wordpress.yaml"
+pma_yaml="$pma_dir""phpmyadmin.yaml"
+cnf_map="service_configmap.yaml"
+
+#	Stopping nginx on localhost
+
+# sudo nginx -s stop
+
+#	Creating TLS certificates
+
+# mkdir certs
+# cd certs
+# openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -batch -out cert.pem -keyout key.pem
+# chmod 400 key.pem
+# cp *.pem ../nginx/srcs
+# cd ..
 
 #	Minikube setup
-#minikube delete
-#minikube start --driver=docker
-eval $(minikube -p minikube docker-env) #in order for minikube to look for docker images locally
+# minikube delete
+# minikube start --driver=docker
+#eval $(minikube -p minikube docker-env) #in order for minikube to look for docker images locally
 
 #Metallab install and start
 
-sh ./metallb_install.sh
- kubectl describe service/kubernetes | grep -i endpoints | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"
+# sh ./metallb/metallb_install.sh
+k8s_ip=$(kubectl describe service/kubernetes | grep -i endpoints | grep -E -o "(([0-9]{1,3}[\.]){3})([0-9]{1,3}){1}")
+echo $k8s_ip
+k8s_ip_last=$(echo $k8s_ip | grep -E -o "([^.]*$)")
+echo $k8s_ip_last
+k8s_ip_last=$((k8s_ip_last + 1))
+echo $k8s_ip_last
+k8s_ip=$(echo $k8s_ip | grep -E -o "(([0-9]{1,3}[\.]){3})")
+echo $k8s_ip
+k8s_ip="$k8s_ip$k8s_ip_last"
+echo $k8s_ip
+#sed -E -i "s/(([0-9]{1,3}[\.]){3}[0-9]{1,3}{1})/$k8s_ip/g" $mld_yaml
+sed -i s/'  ext_ip:'/"  ext_ip: ""$k8s_ip"/ $cnf_map
 
-kubectl apply -f metallb.yaml #need to get IP address first 
+# kubectl apply -f $mld_yaml #need to get IP address first 
 
 #image build
-docker build nginx/ -t nginx-service	 #nginx image build
-docker build wordpress/ -t wordpress-service
-docker build phpmyadmin/ -t phpmyadmin-service
-docker build mysql/ -t mysql-volume
+# docker build $ngx_dir -t nginx-service	 #nginx image build
+# docker build $wp_dir -t wordpress-service
+# docker build $pma_dir -t phpmyadmin-service
+# docker build $msq_dir -t mysql-volume
 
 #docker run --rm -d -p 80:80 -p 443:443 nginx-service
 #docker run --rm -d -p 5050:5050 wordpress-service
@@ -36,6 +65,7 @@ docker build mysql/ -t mysql-volume
 
 #Cluster deployments
 
-kubectl apply -f nginx/depl_nginx.yaml #nginx service deployment
+# kubectl apply -f $ngx_yaml
+# kubectl apply -f $wp_yaml
 
-minikube dashboard & #open kubernetes dashboard
+#minikube dashboard & #open kubernetes dashboard
