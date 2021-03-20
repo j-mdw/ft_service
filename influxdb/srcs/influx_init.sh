c691345f1709674f6a7ed1950919ab4ed8dd8e75
jmaydew <jmaydew@student.42.fr>
2021-03-20 15:11:01 +0100

    Ongoing telegraf config, need to mount docker socket and automate grafana config

--- influxdb/srcs/influx_init.sh
+++ influxdb/srcs/influx_init.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+influxd &
+
+sleep 3
+
 influx << EOF
 create database telegrafdb
 create user telegraf with password 'password'
 grant all on telegrafdb to telegraph
 EOF
+
+telegraf --config /etc/telegraf.conf.d/telegraf.conf
+#--config /etc/telegraf.conf
+
+wait
